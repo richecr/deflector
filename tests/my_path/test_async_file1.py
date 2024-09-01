5fce12e22f193a7da0386d2e60d056afb4fde0bc
Rich Ramalho <richecr1799@gmail.com>
2024-08-31 23:32:45 -0300

    test: added before each tests

--- tests/my_path/test_async_file1.py
+++ tests/my_path/test_async_file1.py
@@ -1,5 +1,6 @@
 import asyncio
 
+from sentinel.helpers.before_each import before_each
 from sentinel.helpers.describe import describe
 from sentinel.helpers.it import it
 from sentinel.modules.essentials.ensure import ensure
@@ -12,15 +13,26 @@ async def main() -> int:
 
 @describe("Main 1")
 def describe_main() -> None:
+    x = 1
+
+    @before_each()
+    async def start() -> bool:
+        nonlocal x
+        await asyncio.sleep(1)
+        x += 1
+        return True
+
     @it("It Test 1")
     async def test1() -> None:
         v = await main()
+        ensure.equal(x, 2, "Equal Before Each")
         ensure.equal(v, 2, "Equal 1")
         ensure.equal(v, 2, "Equal 2")
 
     @it("It Test 2")
     async def test2() -> None:
         v = await main()
+        ensure.equal(x, 3, "Equal Before Each")
         ensure.equal(v, 2, "Equal")
         ensure.not_equal(v, 1, "Not Equal")
         ensure.ok(True, "Ok")
